3768102ec0f8a7240281042854cd0cba15d9f1ff
Jeff Goeders <jeff.goeders@gmail.com>
2020-12-12 17:10:46 -0700

    code cleanup

--- pygrader/student_repos.py
+++ pygrader/student_repos.py
@@ -3,53 +3,7 @@ import shutil
 import subprocess
 import sys
 
-from . import utils
-
-from .utils import error, print_color, TermColors
-
-# from .paths import repos_path, code_checker_path, root_path
-
-
-# def get_students(csv_path):
-#     # Read CSV
-#     if not csv_path.is_file():
-#         error("CSV file", csv_path, "does not exist.")
-#     df = pandas.read_csv(csv_path)
-#     if df.columns[0] != "Last Name":
-#         error("Column 0 of CSV should be 'Last Name', but it is", df.columns[0])
-#     if df.columns[1] != "First Name":
-#         error("Column 1 of CSV should be 'First Name', but it is", df.columns[1])
-#     if df.columns[2] != "Net ID":
-#         error("Column 2 of CSV should be 'Net ID', but it is", df.columns[2])
-#     if df.columns[3] != "Github username":
-#         error("Column 3 of CSV should be 'Github username', but it is", df.columns[3])
-#     print_color(TermColors.PURPLE, "There are", len(df.index), "students in csv file.")
-
-#     # Filter by students who have a github username
-#     df = df[df["Github username"].notnull()]
-
-#     return df
-
-
-# def create_repos_dir(force):
-#     # Create student repos dir, deleting first if needed
-#     if repos_path.is_dir():
-#         if not force:
-#             error(
-#                 "Student repo directory,",
-#                 repos_path,
-#                 ", already exists.  Please delete or use the --force option.",
-#             )
-#         else:
-#             print_color(
-#                 TermColors.YELLOW,
-#                 "Deleting existing student repo directory,",
-#                 repos_path,
-#             )
-#             shutil.rmtree(str(repos_path))
-
-#     print_color(TermColors.GREEN, "Creating student repo directory,", repos_path)
-#     repos_path.mkdir()
+from .utils import print_color, TermColors
 
 
 def clone_repo(git_path, tag, student_repo_path):
