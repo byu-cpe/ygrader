cd53d61f229b8024759026ebaec797a03fff21fb
Jeff Goeders <jeff.goeders@gmail.com>
2022-09-06 13:23:00 -0600

    Update pandas version requirement because of bug in pandas?
    Signed-off-by: Jeff Goeders <jeff.goeders@gmail.com>

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup
 setup(
     name="ygrader",
     packages=["ygrader"],
-    version="1.1.6",
+    version="1.1.7",
     description="Grading scripts used in BYU's Electrical and Computer Engineering Department",
     author="Jeff Goeders",
     author_email="jeff.goeders@gmail.com",
     license="MIT",
     url="https://github.com/byu-cpe/ygrader",
-    install_requires=["pandas"],
+    install_requires=["pandas>=1.0.0"],
 )
