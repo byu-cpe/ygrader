0f40f975350e93a5cf03739d86cd60d6f3f1dcda
Jeff Goeders <jeff.goeders@gmail.com>
2023-09-22 08:48:42 -0600

    increase version
    Signed-off-by: Jeff Goeders <jeff.goeders@gmail.com>

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="ygrader",
     packages=["ygrader"],
-    version="1.1.21",
+    version="1.1.22",
     description="Grading scripts used in BYU's Electrical and Computer Engineering Department",
     author="Jeff Goeders",
     author_email="jeff.goeders@gmail.com",
