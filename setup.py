47eb1f7d36113701422f8ceb3f2eff3973932f55
Jeff Goeders <jeff.goeders@gmail.com>
2024-04-10 12:21:06 -0600

    Increase version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="ygrader",
     packages=["ygrader"],
-    version="1.1.25",
+    version="1.1.26",
     description="Grading scripts used in BYU's Electrical and Computer Engineering Department",
     author="Jeff Goeders",
     author_email="jeff.goeders@gmail.com",
